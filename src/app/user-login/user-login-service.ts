07a6b031a752baca47f0ab853c2925ec03ebfc63
chris <104324755+cjurgens17@users.noreply.github.com>
2023-03-21 13:37:33 -0400

    Added Login Service. Routing is working correctly and goes to home page after login request

--- src/app/user-login/user-login-service.ts
+++ src/app/user-login/user-login-service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { UserLogin } from "./user-login";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 
 @Injectable({
@@ -8,7 +11,13 @@ import { Injectable } from "@angular/core";
 
 export class UserLoginService {
 
+    private userUrl = 'http://localhost:8080/user';
+
     constructor (private http: HttpClient) {}
+
+    loginUser(user: UserLogin): Observable<any> {
+        return this.http.post<any>(`${this.userUrl}/login`, user, {headers: environment.headers});
+    }
 
 
 
--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserLoginService } from './user-login-service';
 import { UserLogin } from './user-login';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -20,12 +21,27 @@ export class UserLoginComponent implements OnInit {
   postError: boolean = false;
   postErrorMessage: string = '';
 
-  constructor(private userLoginService: UserLoginService) {}
+  constructor(private userLoginService: UserLoginService, private router: Router) {}
 
   onSubmit(form: NgForm) {
     console.log('In onSubmit: ', form.valid);
-
-
+    if(form.valid){
+      this.userLoginService.loginUser(this.userLoginInfo).subscribe({
+        next: data => {
+          console.log('data: ', data);
+          this.router.navigate(['home']);
+          localStorage.setItem('userLoginInfo', JSON.stringify(data));
+          console.log(localStorage.getItem('userLoginInfo'));
+        },
+        error: err => {
+          console.log('error: ', err);
+        } 
+     });
+      console.log('Login successful');  
+    }else{
+      this.postError = true;
+      this.postErrorMessage = "Username or password does not exist."
+    }  
 }
 
 ngOnInit(): void { 
