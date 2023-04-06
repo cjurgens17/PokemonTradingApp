d70c637e574b83083b423ab2c5142fe917a8c108
chris <104324755+cjurgens17@users.noreply.github.com>
2023-04-06 16:15:23 -0400

    added styling to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  title = 'Welcome To Pokemon Trading';
+  title = 'Pokemon Trading';
 
   constructor() { }
 
