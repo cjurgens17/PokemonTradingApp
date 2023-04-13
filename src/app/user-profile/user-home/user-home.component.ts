e8c95a7dd4ff19831740d7596b29510cc4cb15a4
chris <104324755+cjurgens17@users.noreply.github.com>
2023-04-13 13:55:17 -0400

    Added filteredPokemon Observable which filters user pokemon on profile page

--- src/app/user-profile/user-home/user-home.component.ts
+++ src/app/user-profile/user-home/user-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { User } from 'src/app/user-info/user-info';
+import { BehaviorSubject, EMPTY, Observable, Subject, Subscription, catchError, combineLatest, filter, map, startWith, tap } from 'rxjs';
 import { UserProfileService } from '../user-profile.service';
+
 
 @Component({
   selector: 'app-user-home',
@@ -13,13 +13,52 @@ export class UserHomeComponent {
   showPokemon: boolean = false;
   errorMessage: string = '';
 
+  private _listFilter: string = '';
+
+  get listFilter(): string{
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log(this.listFilter);
+  }
+
+  //find out how to pass listfilter into action stream to filtered products updates
+
+  //Action stream for getting user input to search for a pokemon
+  private filteredPokemonInputSubject = new Subject<string>();
+  filteredPokemonInput$ = this.filteredPokemonInputSubject.asObservable();
+
+  //Action Stream for handling errors
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   currentUser$ = this.userProfileService.currentUser$;
+  userPokemon$ = this.userProfileService.userPokemon$;
+
+//start with operator????
+  filteredPokemon$ = combineLatest([
+    this.userPokemon$,
+    this.filteredPokemonInput$
+  ]).pipe(
+    map(([userPokemon, filteredInput]) =>
+    userPokemon.filter((pokemon) => pokemon.name.toLowerCase().includes(filteredInput))),
+    tap(data => console.log('Filrtered POkemon: ', data)),
+    catchError( err => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  )
 
   constructor(private userProfileService: UserProfileService) { }
 
   togglePokemon(): void {
     this.showPokemon = !this.showPokemon;
+  }
+
+  onFilterChange() {
+    this.filteredPokemonInputSubject.next(this._listFilter);
   }
 }
 
