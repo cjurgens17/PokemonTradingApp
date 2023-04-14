24e571f231a4d92055be7bba8c15a9a896283875
chris <104324755+cjurgens17@users.noreply.github.com>
2023-04-14 10:58:12 -0400

    Added More styling and deleted pokemon popup card

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
-import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { PokemonCardsComponent } from './pokemoncards/pokemoncards.component';
 import { RouterModule } from '@angular/router';
-import { PokemonCardGuard } from './pokemon-card/pokemon-card.guard';
 import { SharedModule } from '../shared/shared.module';
 import { UserInfoModule } from '../user-info/user-info.module';
 
 @NgModule({
   declarations: [
-    PokemonCardComponent,
     PokemonCardsComponent
   ],
   imports: [
     RouterModule.forChild([
-      {path: 'pokemon', component: PokemonCardsComponent},
-      {
-        path: 'pokemonCard/:name',
-        canActivate: [PokemonCardGuard],
-        component: PokemonCardComponent
-        }
+      {path: 'pokemon', component: PokemonCardsComponent}
     ]),
     SharedModule,
     UserInfoModule
