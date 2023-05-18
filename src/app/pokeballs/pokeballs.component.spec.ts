a4bf32917f89e97f5e1e5a64593194ea3b0f4b6b
chris <104324755+cjurgens17@users.noreply.github.com>
2023-05-18 17:14:33 -0400

    Starting to build jasmine and karma test suites. Created test for pokeballs service and pokeballs component

--- src/app/pokeballs/pokeballs.component.spec.ts
+++ src/app/pokeballs/pokeballs.component.spec.ts
@@ -1,23 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokeballsComponent } from './pokeballs.component';
+import { PokeballsService } from './pokeballs.service';
+import { of } from 'rxjs';
+import { Timer } from './timer';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('PokeballsComponent', () => {
   let component: PokeballsComponent;
   let fixture: ComponentFixture<PokeballsComponent>;
+  let mockPokeBallsService: jasmine.SpyObj<PokeballsService>;
+  let mockSnackBar: jasmine.SpyObj<MatSnackBar>;
 
   beforeEach(async () => {
+     mockPokeBallsService = jasmine.createSpyObj<PokeballsService>('PokeballsService', [
+      'updateUserPokeBalls',
+      'updateTimer',
+      'updateTimerSubject',
+    ]);
+
+    mockSnackBar = jasmine.createSpyObj<MatSnackBar>('MatSnackBar', ['open']);
+
     await TestBed.configureTestingModule({
-      declarations: [ PokeballsComponent ]
+      declarations: [ PokeballsComponent ],
+      providers: [
+        { provide: PokeballsService, useValue: mockPokeBallsService },
+        {provide: MatSnackBar, useValue: mockSnackBar}],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PokeballsComponent);
     component = fixture.componentInstance;
+    component.userId = 1;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update Pokemon Balls and update timer', () => {
+    //arrange
+    const pokeBallCount = 10;
+    const timerValue: Timer = {
+      id: 1,
+      prevDate: new Date(Date.now())
+    }
+    ;
+    const returnTimerValue : Timer = {...timerValue, prevDate: new Date(timerValue.prevDate.getDate() + 1)};
+
+    //Act
+    mockPokeBallsService.updateUserPokeBalls.and.returnValue(of(10));
+    mockPokeBallsService.updateTimer.and.returnValue(of(returnTimerValue));
+
+    component.resetPokemonBalls();
+
+    //Assert
+    expect(mockPokeBallsService.updateUserPokeBalls).toHaveBeenCalledWith(component.userId, pokeBallCount);
+    expect(mockPokeBallsService.updateTimer).toHaveBeenCalledWith(component.userId);
+    expect(mockPokeBallsService.updateTimerSubject).toHaveBeenCalledWith(returnTimerValue);
+    expect(mockSnackBar.open).toHaveBeenCalled();
+  });
+
+
+
 });
--- src/app/pokeballs/pokeballs.service.spec.ts
+++ src/app/pokeballs/pokeballs.service.spec.ts
@@ -1,16 +1,69 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { PokeballsService } from './pokeballs.service';
+import { Timer } from './timer';
+
+
 
 describe('PokeballsService', () => {
   let service: PokeballsService;
+  let httpTestingController: HttpTestingController;
+  let apiUrl = 'http://localhost:8080/timer';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PokeballsService],
+    });
     service = TestBed.inject(PokeballsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-});
+//--------------------
+  describe('getTimer', () => {
+    it('should retrieve the timer from the API', () => {
+      const id = 1;
+      const mockTimer: Timer = {
+        id: 1,
+        prevDate: new Date(),
+      };
+
+      service.getTimer(id).subscribe((timer) => {
+        expect(timer).toEqual(mockTimer);
+      });
+
+      const req = httpTestingController.expectOne(`${apiUrl}/${id}/getTimer`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockTimer);
+    });
+  });
+//---------------
+  describe('updateTimer', () => {
+    it('should update the timer through the API', () => {
+      const id = 123;
+      const mockTimer: Timer = {
+        id: 1,
+        prevDate: new Date(),
+      };
+
+      service.updateTimer(id).subscribe((timer) => {
+        expect(timer).toEqual(mockTimer);
+      });
+
+      const req = httpTestingController.expectOne(`${apiUrl}/${id}/updateTimer`);
+      expect(req.request.method).toBe('POST');
+      req.flush(mockTimer);
+    });
+    });
+
+  });
+
+  // Add more test cases for the remaining methods of the service
+
