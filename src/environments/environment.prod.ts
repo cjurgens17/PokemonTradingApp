3085da258e735985d339e1654ccb78809681ec93
chris <104324755+cjurgens17@users.noreply.github.com>
2023-05-25 17:34:01 -0400

    Getting readt for deployment, updated evirnoment production ts file and updated configuration to allow a larger sized build up to 1.5mb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  withCredentials: true,
+  baseUrl: 'https://pokemon-server.herokuapp.com/',
+  headers: {
+    'content-type' : 'application/json',
+    'Access-Control-Allow-Origin': 'https://pokemon-trading.herokuapp.com/'
+  }
 };
