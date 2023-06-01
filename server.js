e90bd9ba119fc1153f26b3d7b00b9736f3e39cad
chris <104324755+cjurgens17@users.noreply.github.com>
2023-06-01 15:28:55 -0400

    more changes to deployment

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/pokemontrading-frontend'));
+app.use(express.static('./dist/poolwebsite_frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/pokemontrading-frontend/'}),
+    res.sendFile(path.join(__dirname, 'dist/poolwebsite_frontend','index.html')),
 );
 
 // Start the app by listening on the default Heroku port
